46c8a9cdb7f2a874027f7b0f671f28aef507f293
NotJustJoe <furienocturn@hotmail.fr>
2021-03-05 18:18:44 +0100

    -ft_lstclear fixed

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -16,12 +16,11 @@ void	ft_lstclear(t_list **lst, void (*del) (void *))
 {
 	t_list	*ptr;
 
-	ptr = *lst;
-	while (ptr)
+	while (*lst)
 	{
 		ptr = *lst;
 		(del)(ptr->content);
-		ptr = ptr->next;
+		*lst = ptr->next;
 		free(ptr);
 	}
 	(*lst) = NULL;
